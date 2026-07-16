95cdb6bc6a73aedfcf8948fd897d6f5fed697e06
Бабурин Дмитрий <graminstt@gmail.com>
2026-07-16 21:23:52 +0500

    точка входа в программу в файле main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,21 +1,27 @@
 /*
-lib_main.c - реализация функций библиотеки (только для Windows).
+lib_main.c - реализация функций библиотеки. Вызов функций инициализации, парсинга и генерации
 
 Бабурин Дмитрий Сергеевич
 МК-101
 */
 
+#include "lib_main.h"
+#include <stdlib.h>
 
-#include <lib_main.h>
- 
-#include <stdio.h>
-
-
-int main (int argc, char *argv[], char *envp[]) {
-
-int a = 2;
-int b = 3;
-
-	printf ("%d + %d = %d\n", a, b, LibAddFunction(a, b));
-	return 0;
+int main(int argc, char **argv) {
+    PasswordConfig config;
+    
+    // Инициализируем структуру конфигурации дефолтными значениями
+    init_config(&config);
+    
+    // Парсим аргументы командной строки
+    if (parse_arguments(argc, argv, &config) != 0) {
+        // Если парсер вернул ошибку, завершаем программу с кодом неуспеха
+        return EXIT_FAILURE;
+    }
+    
+    // Запускаем генерацию паролей
+    generate_passwords(&config);
+    
+    return EXIT_SUCCESS;
 }
